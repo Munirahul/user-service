aa39662a192befff66287606d8a4800e1559764b
Munirahul <80394091+Munirahul@users.noreply.github.com>
2021-05-06 19:03:34 +0530

    Add files via upload

---
+++ user-service/src/main/java/com/cts/training/bean/User.java
@@ -0,0 +1,53 @@
+package com.cts.training.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name = "user_details") //creates user_details table in sql
+public class User {
+		
+		@Id //it makes id as a primary key
+		//@Column(name = "user_details") //this is used to make that particular column as primary key
+		
+		private Long id;
+		private String username;
+		private String password;
+		private String address;
+		private Long phone;
+		
+		
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public Long getPhone() {
+			return phone;
+		}
+		public void setPhone(Long phone) {
+			this.phone = phone;
+		}
+
+}
---
+++ user-service/target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into user (id, username,password,address,phone) values(1001L,'Rahul','rahul123','Nellore',9618206456L);
+insert into user (id, username,password,address,phone) values(1002L,'Ayshu','Ayshu#','Nellore',7090890765L);
+insert into user (id, username,password,address,phone) values(1003L,'Sumanth','Suman@','pune',9618298767L);
+insert into user (id, username,password,address,phone) values(1004L,'Anoop','@#$','Banglore',9080706050L);
+insert into user (id, username,password,address,phone) values(1005L,'mahesh','123456','Hyd',6090807050L);
